4bd286ae31020333f285485e9f3d0c244c3cafda
Jasmine Nunez <jasminemeinunez@icloud.com>
2023-08-14 10:38:59 -0400

    Initial Commit

---
+++ AboutMeApp/ContentView.swift
@@ -0,0 +1,34 @@
+//
+//  ContentView.swift
+//  AboutMeApp
+//
+//  Created by clark_loaner on 8/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+   
+    VStack(alignment: .leading, spacing: 20.0) {
+            Text("Jasmine Nunez")
+                .font(.title)
+                .fontWeight(.bold)
+        Image("IMG_6870")
+            .resizable ()
+            .aspectRatio (contentMode : .fit)
+            .cornerRadius (15)
+    }
+   
+
+        
+        
+        
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
